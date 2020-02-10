db89f77bdc744da75ee836b8797b1193a3e61d5a
abdulmalikgh <abdul-malik.musah.stu@uenr.edu.gh>
2020-02-10 10:28:55 -0800

    Render books from BookShelf to UI

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -5,24 +5,26 @@ function BookShelf(props) {
   return (
     <div className="bookshelf-books">
       <ol className="books-grid">
-        <li>
-          <div className="book">
-            <div className="book-top">
-              <div className="book-cover" style={{width: 128, height: 192}}></div>
-              <div className="book-shelf-changer">
-                <select>
-                  <option disabled>Move to...</option>
-                  <option value="currentlyReading">currentlyReading</option>
-                  <option value="wantToRead">Want to Read</option>
-                  <option value="read">Read</option>
-                  <option value="read">none</option>
-                </select>
+          {props.books.map( book => (
+            <li>
+              <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{width: 128, height: 192 , backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                <div className="book-shelf-changer">
+                  <select value={book.shelf}>
+                    <option disabled>Move to...</option>
+                    <option value="currentlyReading">currentlyReading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">none</option>
+                  </select>
+                </div>
               </div>
-            </div>
-            <div className="book-title">The Adventures of Tom Sawyer</div>
-            <div className="book-authors">Mark Twain</div>
-          </div>
-        </li>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+           </div>
+           </li>
+          ))}
       </ol>
     </div> 
 ) 
