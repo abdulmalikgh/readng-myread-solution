da9edb8e58e340c1fe540d060e51e95d9aaed27b
abdulmalikgh <abdul-malik.musah.stu@uenr.edu.gh>
2020-02-10 23:35:53 -0800

    Refactor ReadBooks component

--- src/ReadBooks.js
+++ src/ReadBooks.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import BookShelf from './BookShelf'
+
 function ReadBooks(props) {
  return (
     <div className="bookshelf">
     <h2 className="bookshelf-title">Read</h2>
-      <BookShelf books={props.books} />
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+          {props.books.map( book => (
+            <li key={book.id}>
+              <div className="book">
+              <div className="book-top">
+                <div className="book-cover" style={{width: 128, height: 192 , backgroundImage:`url(${book.imageLinks.smallThumbnail})`}}></div>
+                <div className="book-shelf-changer">
+                  <select value={book.shelf} onChange={props.handleChange(book.id)}>
+                    <option disabled>Move to...</option>
+                    <option value="currentlyReading">currentlyReading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">none</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors}</div>
+           </div>
+           </li>
+          ))}
+      </ol>
+    </div> 
     </div>
  )
 }
